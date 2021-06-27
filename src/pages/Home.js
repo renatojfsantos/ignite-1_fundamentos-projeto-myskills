17f5ce321976dbcb1e6593649656e581d0e16e79
Renato Santos <renato_jfs@hotmail.com>
2021-06-27 00:36:16 -0300

    Chapter 1 - Conceitos importantes - Continuando com Hooks useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   View,
   Text, 
@@ -15,16 +15,35 @@ export function Home() {
 
   const [newSkill, setNewSkill] = useState('');
   const [mySkills, setMySkills] = useState([]);
+  const [gretting, setGretting] = useState('');
 
   function handleAddNewSkill() {
     setMySkills(oldState => [...oldState, newSkill]);
   }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if(currentHour < 12) {
+      setGretting('Good morning');
+    }
+    else if(currentHour >=12 && currentHour <18) {
+      setGretting('Good afternoon');
+    } else {
+      setGretting('Good night');
+    }
+
+  }, [])
 
   return (
     <View style={styles.container}>
       
       <Text style={styles.title}>
         Welcome, Renato
+      </Text>
+
+      <Text style={styles.greetings}>
+        { gretting }
       </Text>
 
       <TextInput
@@ -74,6 +93,10 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 7
+  },
+
+  greetings: {
+    color: '#FFF',
   },
 
 })

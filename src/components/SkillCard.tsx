2259c9963355ff42fe68342554432d4e83b980fc
Renato Santos <renato_jfs@hotmail.com>
2021-06-29 00:31:39 -0300

    Chapter 1 - Utilizando TypeScript - Tipando componentes com TypeScript

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import {
     TouchableOpacity,
+    TouchableOpacityProps,
     Text,
     StyleSheet
 } from 'react-native';
 
-export function SkillCard({ skill }) {
+interface SkillCardProps extends TouchableOpacityProps{
+  skill: string;
+}
+
+export function SkillCard({ skill, ...rest } : SkillCardProps) {
     return(
-        <TouchableOpacity style={styles.buttonSkill}>
-            <Text style={styles.textSkill}>
-              {skill}
-            </Text>
-          </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.buttonSkill}
+          {...rest}
+        >
+          <Text style={styles.textSkill}>
+            {skill}
+          </Text>
+        </TouchableOpacity>
     )
 }
 
